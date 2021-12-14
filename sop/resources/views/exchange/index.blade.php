2a8a5da31feb36c1d12a7e90f5efd633607af207
PanMaster13 <36907215+PanMaster13@users.noreply.github.com>
2021-12-14 10:28:12 +0800

    Commit #5: Changed default timezone
    Changed timezone to 'Asia/Kuching'

--- sop/resources/views/exchange/index.blade.php
+++ sop/resources/views/exchange/index.blade.php
@@ -3,5 +3,9 @@
 @section('content')
 <div class="container">
     <h3>Control Exchange Program</h3>
+    <?php  
+        $date = date('m/d/Y h:i:s a', time());
+        echo $date;
+    ?>
 </div>
 @endsection
